ec6ebf184bde31453f09b59449b52cc12d00c412
osipovnikita <bayes@mail.ru>
2023-11-05 15:35:35 +0300

    Добавлена переменная для вызова функции

--- FirstDemo/addFun.py
+++ FirstDemo/addFun.py
@@ -4,5 +4,5 @@ def sum(a, b):
 
 a = int(input('Enter 1nd number: '))
 b = int(input('Enter 2nd number: '))
-
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+c= sum(a,b)
+print(f'Sum of {a} and {b} is {c}')
